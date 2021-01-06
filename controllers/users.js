7b21d7c00da88390c7080af86ec3eaee2c4b42ba
jon apagueno <jonathan.apagueno@gmail.com>
2021-01-05 19:31:49 -0800

    adds first controller action

---
+++ controllers/users.js
@@ -0,0 +1,19 @@
+// require modules
+const User = require('../models/user');
+
+// handle exports
+module.exports = {
+    signup
+};
+
+// define singup controller action
+async function signup(req, res) {
+    try {
+        const user = await User.create(req.body);
+        res.json({ user })
+        const token
+    } catch (error) {
+        res.status(400).json({ msg: 'bad request' });
+    }
+}
+    // create user the send the user document as json
