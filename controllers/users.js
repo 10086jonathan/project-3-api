28d5817c2abda9ed107dd465227ac42f54b072dd
jon apagueno <jonathan.apagueno@gmail.com>
2021-01-06 19:46:48 -0800

    last commit

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,7 @@
 // require modules
 const User = require('../models/user');
+const SECRET = process.env.SECRET;
+const jwt = require('jsonwebtoken');
 
 // handle exports
 module.exports = {
@@ -10,9 +12,16 @@ module.exports = {
 async function signup(req, res) {
     try {
         const user = await User.create(req.body);
-        res.json({ user });
+
+        const token = createJWT(user);
+
+        res.json({ token });
+        
     } catch (error) {
         res.status(400).json({ msg: 'bad request' });
     }
 }
-    // create user the send the user document as json
+
+function createJWT(user) {
+    return jwt.sign({ user }, SECRET, {expiresIn: '24h'})
+}
